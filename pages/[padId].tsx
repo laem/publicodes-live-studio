ea85be5e740f2427025e1f20b854739e8d70a273
Mael <mael.thomas@data.gouv.fr>
2023-01-06 16:56:04 +0100

    Index dynamique, pas besoin de sous-chemin

--- pages/[padId].tsx
+++ pages/[padId].tsx
@@ -1,18 +1,22 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const Card = styled.div`
   background: cyan;
 `;
 
-export default function Home() {
+export default function Home({}) {
+  const router = useRouter();
+  const { padId } = router.query;
+
   return (
     <div>
       <Head>
         <title>Studio publicodes live</title>
         <link rel="icon" href="/logo-publicodes.svg" />
       </Head>
-      <Card>YO</Card>
+      <Card>YO {padId}</Card>
       <div
         css={`
           background: red;
