7616fca603bb9b6dd4f327966063bbd320873998
Mael <mael.thomas@data.gouv.fr>
2023-01-06 12:24:31 +0100

    Init

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Studio publicodes live</title>
+        <link rel="icon" href="/logo-publicodes.svg" />
+      </Head>
+
+      <main>
+        <h1>Studio publicodes live</h1>
+      </main>
+    </div>
+  );
+}
+
