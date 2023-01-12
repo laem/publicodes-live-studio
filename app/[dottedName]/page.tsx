ce7680459a1969d3e34c5df39f92db08bc38adc6
Mael <mael.thomas@data.gouv.fr>
2023-01-12 12:08:52 +0100

    Test de début de page /documentation

--- app/[dottedName]/page.tsx
+++ app/[dottedName]/page.tsx
@@ -1,3 +1,35 @@
-export default ({ params: { dottedName } }) => (
-  <div>Ma page de doc {dottedName}</div>
-)
+'use client'
+import Link from 'next/link'
+import Engine from 'publicodes'
+import { RulePage } from 'publicodes-react'
+
+const engine = new Engine({ a: 2, b: 'a + 10', 'b . c': 'a * b' })
+
+export default function Page({ params: { dottedName } }) {
+  return (
+    <div>
+      <h1>Ma page de doc pour {dottedName} </h1>
+      <DocPage dottedName={dottedName} />
+    </div>
+  )
+}
+
+const DocPage = ({ dottedName }) => {
+  console.log('engineParsedRules:', engine.context.parsedRules)
+
+  return (
+    <div>
+      <RulePage
+        language={'fr'}
+        rulePath={dottedName}
+        engine={engine}
+        documentationPath={'/documentation'}
+        renderers={{
+          Head: () => <title>Le titre de {dottedName}</title>,
+          Link: ({ to }) => <Link href={to} />,
+          Text: ({ children }) => <>Le corps de {dottedName}</>,
+        }}
+      />
+    </div>
+  )
+}
