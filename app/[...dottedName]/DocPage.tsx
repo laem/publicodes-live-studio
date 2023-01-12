22fc4e9e553d457788336ee7eb7facf02597d43f
Mael <mael.thomas@data.gouv.fr>
2023-01-12 17:31:41 +0100

    Ajout de liens pour tester la navigation sur la doc

--- app/[...dottedName]/DocPage.tsx
+++ app/[...dottedName]/DocPage.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+
 export const DocPage = ({ dottedName, evaluation, rule }) => {
   return (
-    <div>
-      {dottedName}
-      {evaluation.nodeValue} {evaluation.title} {rule && rule.description}
-    </div>
+    <>
+      <div>{dottedName}</div>
+      <div>
+        <Link href="/bilan">Bilan</Link>
+        <Link href="/transport/empreinte">Transport</Link>
+        <Link href="/alimentation/plats/viande-1">Alimentation</Link>
+      </div>
+      <div>
+        {evaluation.nodeValue} {evaluation.title}
+      </div>
+      <div>{rule && rule.description}</div>
+    </>
   )
 }
