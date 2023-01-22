1239b666525dbaccc314419e56aaaee2aa91f7ee
Mael <mael.thomas@data.gouv.fr>
2023-01-22 16:24:18 +0100

    Déplacement du texte de /document sur l'accueil

--- app/document/page.tsx
+++ app/document/page.tsx
@@ -1,30 +1,3 @@
-'use client'
-import Link from 'next/link'
-import { useState } from 'react'
-import { generateRoomName } from './[padName]/studioShareUtils'
-
 export default () => {
-  const [name, setName] = useState(generateRoomName())
-  return (
-    <div css="background: cyan">
-      <p>
-        Bienvenu sur le studio publicodes live. Il vous permet de collaborer en
-        temps réel sur un document partagé, stocké sur nos serveurs.
-      </p>
-      <div>
-        <input
-          type="string"
-          style={{ width: '16rem' }}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Le nom de votre document"
-        />
-
-        <button onClick={() => setName(generateRoomName())}>
-          ♻️ Générer un autre nom
-        </button>
-      </div>
-      <Link href={'/document/' + name}>C'est parti !</Link>
-    </div>
-  )
+  return <div>Vous devez spécifier un nom de document dans l'URL.</div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
-// Import your Client Component
+'use client'
 import Link from 'next/link'
+import { useState } from 'react'
+import { generateRoomName } from './document/[padName]/studioShareUtils'
 
-export default async function Page() {
-  // Forward fetched data to your Client Component
+export default function Page() {
+  const [name, setName] = useState(generateRoomName())
   return (
     <div>
-      <h1>Bienvenue sur le studio publicodes</h1>
-      <Link href="/document">C'est parti</Link>
       <p>
-        Connaissez-vous les pages
-        <Link href="/a">A</Link>
-        et
-        <Link href="/b">B</Link>
+        Bienvenu sur le studio publicodes live. Il vous permet de collaborer en
+        temps réel sur un document partagé, stocké sur nos serveurs.
       </p>
+      <div>
+        <input
+          type="string"
+          style={{ width: '16rem' }}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Le nom de votre document"
+        />
+
+        <button onClick={() => setName(generateRoomName())}>
+          ♻️ Générer un autre nom
+        </button>
+      </div>
+      <Link href={'/document/' + name}>C'est parti !</Link>
     </div>
   )
 }
