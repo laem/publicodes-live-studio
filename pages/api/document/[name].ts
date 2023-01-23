b01ff2db8b4bc83e8dc15ef320fdd25bb13bd765
Mael <mael.thomas@data.gouv.fr>
2023-01-23 11:54:33 +0100

    Fantastique ! C'est l'événement 'synced' qu'il me manquait

--- pages/api/document/[name].ts
+++ pages/api/document/[name].ts
@@ -16,39 +16,18 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       WebSocketPolyfill: ws,
     })
 
-    // HACK : I don't know why onConnect() is run twice
-    let connectFunctionFired = false
-
     return new Promise(async (resolve) => {
-      const onConnect = () => {
-        if (connectFunctionFired) return
-        connectFunctionFired = true
+      const onSynced = () => {
         const text = provider.document.getText('monacoCode')
         // HACK : how to know if empty ? Know when fully loaded ?
-        const timeoutFunction = setTimeout(() => {
-          if (!text.length) {
-            provider.off('connect', onConnect)
-            provider.disconnect()
-            clearTimeout(timeoutFunction)
-            res.status(200).json({ name, content: null })
-            return resolve()
-          }
-        }, 500)
-
-        const observeFunction = (event) => {
-          const content = parse(text.toJSON())
-
-          text.unobserve(observeFunction)
-          provider.off('connect', onConnect)
-          provider.disconnect()
-          clearTimeout(timeoutFunction)
-
-          res.status(200).json({ name, content })
-          return resolve()
-        }
-        return text.observe(observeFunction)
+        provider.off('synced', onSynced)
+        provider.disconnect()
+        res
+          .status(200)
+          .json({ name, content: text.length ? parse(text.toJSON()) : null })
+        return resolve()
       }
-      return provider.on('connect', onConnect)
+      return provider.on('synced', onSynced)
     })
   } catch (error) {
     return res.status(405).json(error)
